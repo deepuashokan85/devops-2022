199845edeb3fc2039a0b930279717ecf913e746b
Kavithagodwin <kavithaeceg10@gmail.com>
2022-04-09 09:05:56 +0530

    session

---
+++ practice/main.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "test" {
+ ami	= var.ami_id
+ instance_type	= var.inst_type
+ subnet_id	= var.sub_id
+ count	= 2
+
+ tags = {
+  Name = "kavitha-terraform"
+ } 
+}
+
+resource "security-group" "kavitha-sg" {
+ name = "kavitha-sg"
+ description = "Allow TLS inbound traffic"
+ vpc_id = "vpc-06619b9a4856571a7"
+ 
+ ingress {
+  description = "TLS from vpc"
+  from_port = 443
+  to_port = 443 
+  protocol = "tcp"
+  cidr_block = ["10.10.0.0/32"]
+ }
+
+ egress {
+  from_port = 0
+  to_port = 0
+  protocol = -1
+  cidr_block = ["0.0.0.0/0"]
+ }
+
+ tags = {
+  Name = "allow http"
+ }
+}
+
---
+++ practice/variable.tf
@@ -0,0 +1,3 @@
+variable ami_id {}
+variable inst_type {}
+variable sub_id {}
