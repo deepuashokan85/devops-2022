f81a3ab4dfbe5a207f570f4960e8524c2f39c730
EC2 Default User <ec2-user@ip-172-31-12-165.ec2.internal>
2022-04-16 16:55:29 +0000

    terraform module assignment

--- terraform-module/dev/main.tf
+++ terraform-module/dev/main.tf
@@ -1,26 +1,61 @@
-module "aws_web" {
-  source           = "../modules/ec2"
-  ami_id	   = "ami-0c02fb55956c7d316"
-  inst_type        = "t2.micro"
-  sub_id	   = "subnet-09484583fb93f28db"
-  count_id	           = 2
-  tag    	   = "dev-pradeep-web"
+module "aws_network" {
+  source = "../modules/ec2"
+  cidr = "192.64.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["192.64.4.0/24", "192.64.5.0/24", "192.64.6.0/24"]
+  public_subnets  = ["192.64.1.0/24", "192.64.2.0/24", "192.64.3.0/24"]
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  igw_id = "igw-0c8db5797e30a89bd"
+  ngw_id = "nat-0fc785c22a36a416b"
+  route_id = "rtb-03b5fb9c4c98a9e67"
+  count_id = 2
+  tags = "dev-balaaji-vpc"
+  }
+
+module "aws_loadbalancer" {
+  source = "../modules/ec2"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = "sg-0906822d667dabc45"
+  subnets            = ["192.64.1.0/24", "192.64.2.0/24", "192.64.3.0/24"]
+  count_id = 2
+  enable_deletion_protection = true
+  access_logs {
+    bucket  = "balaji-bucket-statefile1"
+    prefix  = "test-lb"
+    enabled = true
+  }
+  tags = "dev-balaaji-lb"
 }
 
-module "aws_app" {
+
+module "aws_ec2" {
   source           = "../modules/ec2"
-  ami_id           = "ami-0c02fb55956c7d316"
+  ami_id           = "ami-03ededff12e34e59e"
   inst_type        = "t2.micro"
-  sub_id           = "subnet-09484583fb93f28db"
-  count_id            = 3
-  tag              = "dev-pradeep-app"
+  sub_id	   = "subnet-0bcd966a31abfb6c3"
+  count_id         = 1
+  tag              = "dev-balaaji-ec2"
 }
 
-module "aws_db" {
-  source           = "../modules/ec2"
-  ami_id           = "ami-0c02fb55956c7d316"
-  inst_type        = "t2.micro"
-  sub_id           = "subnet-09484583fb93f28db"
-  count_id            = 1
-  tag              = "dev-pradeep-db"
-}
+module "aws_sg" {
+  idescription = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0fe99ec612ea82f11"
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["10.10.10.10/32"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  count_id         = 1
+  tags = "dev-balaaji-sg"
+  }
+
--- terraform-module/modules/ec2/main.tf
+++ terraform-module/modules/ec2/main.tf
@@ -1,9 +1,19 @@
 resource "aws_instance" "devops-2022" {
-  ami           = var.ami_id
-  instance_type = var.inst_type
-  subnet_id   = var.sub_id
-  count = var.count_id
-
+  cidr_id             = var.cidr_id
+  azs_id              = var.azs_id
+  private_subnets_id  = var.private_subnets_id
+  public_subnets_id   = var.public_subnets_id
+  igw_id              = var.igw_id
+  ngw_id              = var.ngw_id
+  route_id            = var.route_id
+  count_id            = var.count_id  
+  security_groups_id  = var.security_groups_id
+  bucket_id           = var.bucket_id
+  ami_id              = var.ami_id
+  instance_type       = var.inst_type
+  subnet_id           = var.sub_id
+  count               = var.count_id
+  vpc_id              = var.vpc_id
   tags = {
     Name = var.tag
   }
