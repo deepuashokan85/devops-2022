7d2be755d8690f4579e4be17c45f685bab6a2493
Balaraja-k <100459450+Balaraja-k@users.noreply.github.com>
2022-04-16 20:45:19 +0530

    Update backend.tf

--- terraform-module1/terraform-module/stage/backend.tf
+++ terraform-module1/terraform-module/stage/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pradeep-bucket-statefile1"
+    bucket = "balaji-bucket-statefile1"
     key    = "stage-moduledemo-statefile/terrform.tfstate"
     region = "us-east-1"
   }
